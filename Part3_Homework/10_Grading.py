63d3638510edcf9e25c2b224701fb8ae90ffdc3d
mango713 <marco.mladin05@e-uvt.ro>
2025-11-20 09:38:16 +0000

    hello

--- Part3_Homework/10_Grading.py
+++ Part3_Homework/10_Grading.py
@@ -8,15 +8,15 @@
 # 60-69: D
 # Below 60: F
 
-#score = int(input("Enter the student's score: "))
+score = int(input("Enter the student's score: "))
 
-#if score >= 90:
+if score >= 90:
     print("Grade: A")
-#elif score >= 80:
+elif score >= 80:
     print("Grade: B")
-#elif score >= 70:
+elif score >= 70:
     print("Grade: C")
-#elif score >= 60:
+elif score >= 60:
     print("Grade: D")
-#else:
+else:
     print("Grade: F")
